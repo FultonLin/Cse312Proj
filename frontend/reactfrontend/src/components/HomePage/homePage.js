74ad05a622d6a833d4011f9a97acb9291210eff5
Jordan Clemons <jaclemon@buffalo.edu>
2021-03-05 22:30:59 -0500

    Finished frontend calendar component

--- frontend/reactfrontend/src/components/HomePage/homePage.js
+++ frontend/reactfrontend/src/components/HomePage/homePage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './homePage.css'
 
+import Calendar from './Calendar/calendar'
+
 function HomePage() {
   return (
     <div className="Home-container">
         <h1 className="Home-title">Calendarify.</h1>
         <div className="Home-content-container">
           <div className="Calendar-container">
-            <h1>Calendar component goes here</h1>
+            <Calendar/>
           </div>
           <div className="Social-container">
             <h1>Social and Dms go here</h1>
