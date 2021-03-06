c0746d86508edc23369bc0d663eedb00f7c97796
Jordan Clemons <jaclemon@buffalo.edu>
2021-03-06 13:15:56 -0500

    Removed uneccesary frontend code

--- frontend/reactfrontend/src/components/HomePage/Social/onlinePicture.js
+++ frontend/reactfrontend/src/components/HomePage/Social/onlinePicture.js
@@ -5,7 +5,7 @@ import defaultProfile from '../../../images/default-profile.png'
 function OnlinePicture({username}) {
   return (
     <div className="online-profile-box">
-        <img src={defaultProfile} className="profile-picture-online"/>
+        <img src={defaultProfile} className="profile-picture-online" alt="Profile avatar"/>
         <p1>{username}</p1>
     </div>
   );
