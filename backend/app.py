c019cc0b4efb36083f89f2eedf4f4ab7c811e519
Fulton Lin <fultonli@buffalo.edu>
2021-03-09 15:04:29 -0500

    more

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route ('/')
 def home():
-    return app.send_static_file('index.html')
+    return render_template('/reactfrontend/public/index.html')
 
 @app.route('/time')
 def get_current_time():
