264cc454f8e406d8fd5e3405a2168aee9dcc674c
Fulton Lin <fultonli@buffalo.edu>
2021-03-09 13:42:48 -0500

    setup dockerfiles

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route ('/')
+def home():
+    return app.send_static_file('index.html')
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
