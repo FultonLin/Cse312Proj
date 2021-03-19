cc254b662b6460d0da0cd563fcd3d3d4602c61b6
Fulton Lin <fultonli@buffalo.edu>
2021-03-19 18:47:10 -0400

    Send form data and hash pass

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 import time
-from flask import Flask
+from flask import Flask,request
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__, static_folder='../app/build', static_url_path='/')
+bcrypt = Bcrypt()
 
 @app.errorhandler(404)
 def not_found(e):
@@ -12,6 +14,17 @@ def not_found(e):
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/app/time')
-def get_current_time():
-    return {'time':time.time()}
+# @app.route('/app/time')
+# def get_current_time():
+#     return {'time':time.time()}
+
+@app.route('/app/create', methods=['POST'])
+def create():
+    print("THIS IS A TEST",flush=True)
+    data = request.get_json(force=True)
+    username = data.get('username', None)
+    email = data.get('email', None)
+    hashpass = bcrypt.generate_password_hash(data.get('password', None))
+    print(hashpass)
+    print(data)
+    return app.send_static_file('index.html')
