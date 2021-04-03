423b5a0de701fd3079588f85cf6e3ede7c83479e
JordanClemons <jaclemon@buffalo.edu>
2021-04-03 02:23:57 -0400

    Added navigation buttons on webapp

--- frontend/src/Components/LobbyPage/lobbyPage.js
+++ frontend/src/Components/LobbyPage/lobbyPage.js
@@ -4,10 +4,18 @@ import './lobbyPage.css'
 
 import CalendarBubble from './lobbyComponents/CalendarBubble'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser} from '@fortawesome/free-solid-svg-icons'
+
 function LoginPage() {
   return (
     <div className="Lobby-container">
-        <h1 className="Lobby-title">Calendarify.</h1>
+        <div className="Lobby-headers">
+          <h1 className="Lobby-title">Calendarify.</h1>
+          <div className="Nav-buttons">
+            <div className="Lobby-profile"><FontAwesomeIcon icon={faUser} size="2x"/></div>
+          </div>
+        </div>
         <div className="Lobby-center-container">
             <h1>Your calendars</h1>
             <div className="Lobby-join-create-container">
